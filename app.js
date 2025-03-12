e595c5c9f96c700430278dddc408961ff0436266
Alejandro Bautista <alejandro.hernandez4819@alumnos.udg.mx>
2025-03-12 09:04:08 -0600

    Add: Template

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
-
-var titulo = document.querySelector('h1');
-titulo.innerHTML = 'Hora del Desafío';
-
-function Console(){
-    console.log('El botoón fue clicado');
-}
-
-function Prompt(){
-    var ciudad = prompt('¿De qué ciudad de Brasil eres?');
-    alert('Estuve en ' + ciudad + ' y me acorde de ti');
-}
-
-function Alert(){
-    alert('Yo amo JS');
-}
-
-function Suma(){
-    var num1 = parseInt(prompt('Ingrese un número'));
-    var num2 = parseInt(prompt('Ingrese otro número'));
-    var resultado = num1 + num2;
-    alert('El resultado de la suma es: ' + resultado);
-}
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var names = [];
